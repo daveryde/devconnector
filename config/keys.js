ce3b968881e650cab6a2bfedad008a6ac13869d4
David Cordova <d.mcordova87@gmail.com>
2018-08-26 18:40:54 -0700

    initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    'mongodb://daveryde:Stupid001!@ds131932.mlab.com:31932/devconnector_daveryde'
+};
---
+++ routes/api/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/users/test
+// @desc    Tests users route
+// @access  Pubilc
+router.get('/test', (req, res) => {
+  res.json({
+    msg: 'Users Works'
+  });
+});
+
+module.exports = router;
