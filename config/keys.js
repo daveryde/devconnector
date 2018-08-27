7a44711ce9baa593da50d185d5662ba37f6e14a4
David Cordova <d.mcordova87@gmail.com>
2018-08-26 22:09:12 -0700

    mongoose schema, jwt validation, error checking

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    'mongodb://daveryde:Stupid001!@ds131932.mlab.com:31932/devconnector_daveryde'
+    'mongodb://daveryde:Stupid001!@ds131932.mlab.com:31932/devconnector_daveryde',
+  secretOrKey: 'secret'
 };
