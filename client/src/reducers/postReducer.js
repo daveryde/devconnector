8933cddf08dd3326deae54df5ed74434c2b5bd1d
David Cordova <d.mcordova87@gmail.com>
2018-12-18 18:47:48 -0700

    post reducer and action types created

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  posts: [],
+  posts: {},
+  loading: false
+};
+
+export default function(state = initialState, action) {
+  switch(action.type) {
+    default:
+      return state;
+  } 
+}
